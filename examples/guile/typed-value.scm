cafad53d994bd876e21cad0f8da3712178f03586
Skyler Ferris <skyvine@protonmail.com>
2025-01-20 09:25:18 -0500

    Expand & clarify GOOPS section.
    This section feels a little small. I looked through all the commits that
    mention GOOPS as well as the dev archive. There's a lot of activity,
    it's just not relevant to this paper. The only thing that is relevant is
    that the system prints an error if no type signature matches the given
    arguments.
    
    I expect to have a lot more to say about GOOPS when I write the
    analagous paper about object systems.

--- examples/guile/typed-value.scm
+++ examples/guile/typed-value.scm
@@ -6,7 +6,4 @@
 (define-method (add-values (a <string>) (b <string>))
   (string-append a b))
 
-(define-method (add-values a b)
-  (error (format #f "Unknown types for a (~s) and b (~s)." (class-of a) (class-of b))))
-
 (add-values 1000 1001)
